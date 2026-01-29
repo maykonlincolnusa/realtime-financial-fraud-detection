28830003d1c8b2730ef223fef3793bacdf7c9062
Maykon Lincoln <maykonlincolnmanager@gmail.com>
2026-01-29 14:17:12 -0300

    Create src/api/app.py

---
+++ src/api/app.py
@@ -0,0 +1,9 @@
+# src/api/app.py
+from fastapi import FastAPI
+from src.api.routes import alerts, transactions, graph
+
+app = FastAPI(title="Fraud Detection API")
+
+app.include_router(alerts.router, prefix="/alerts", tags=["Alerts"])
+app.include_router(transactions.router, prefix="/transactions", tags=["Transactions"])
+app.include_router(graph.router, prefix="/graph", tags=["Graph"])
